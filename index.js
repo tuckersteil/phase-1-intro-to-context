b8b0ef67795cf256ad4ede4b8ff15b642cf1d9ae
Tucker Steil <tuckersteil@gmail.com>
2022-04-27 15:32:01 -0400

    commit tree bc156e009d2ea5ae33a051b7be3c85a2e0d3fab6

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Your code here
+// let twoRows = [
+//     ["moe", "sizlak", "barkeep", 2],
+//     ["bartholomew", "simpson", "scamp", 3]
+//   ]
+
+function createEmployeeRecord(employee){
+    let newObj = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+     return newObj
+}
+
+function createEmployeeRecords(employees){
+    return employees.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(obj, dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+    hour = parseInt(hour)
+    let type = "TimeIn";
+    obj.timeInEvents.push({type, hour, date});
+    return obj
+}
+
+function createTimeOutEvent(obj, dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+    hour = parseInt(hour)
+    let type = "TimeOut";
+    obj.timeOutEvents.push({type, hour, date});
+    return obj
+}
+
+function hoursWorkedOnDate(obj, workDate){
+    let clockOut = obj.timeOutEvents
+        .filter((element) => element.date === workDate)
+        .map((element) => element.hour);
+
+    let clockIn = obj.timeInEvents
+        .filter((element) => element.date === workDate)
+        .map((element) => element.hour);
+
+    return (clockOut - clockIn)/100
+}
+
+function wagesEarnedOnDate(obj, date){
+    return obj.payPerHour * hoursWorkedOnDate(obj, date)
+}
+
+function allWagesFor(obj){
+    let result = [];
+    const allDates = obj.timeInEvents.map((element) => (element = element.date));
+    for (let element of allDates) {
+        result.push(wagesEarnedOnDate(obj, element));
+    }
+    return result.reduce((a, b) => a + b, 0)
+}
+
+function calculatePayroll(array){
+    return array.map(obj => allWagesFor(obj))
+    .reduce((a, b) => (a = a + b), 0)
+}
